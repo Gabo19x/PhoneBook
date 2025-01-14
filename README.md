1d88200c88d551d0ca2e19837ffdb80d40a96798
Gabo19 <arturopinzon19@gmail.com>
2025-01-14 18:48:03 -0500

    Add /info with date and contacts

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 The database is added but in JS, and placed in /api/persons
 
 ### 3.2
+In /info the date and number of contacts are shown
--- index.js
+++ index.js
@@ -2,13 +2,28 @@ const express = require("express");
 const app = express();
 const {DB} = require("./data base/DB.js");
 
-app.get("/", (req, res) => {
-    res.send("Hello!!!");
-});
-
 app.get("/api/persons", (req, res) => {
     console.log("GET personas");
     res.status(200).json(DB);
+});
+
+app.get("/info", (req, res) => {
+    const tamaño = DB.length;
+
+    res.send(`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>PHONE BOOK</title>
+        </head>
+        <body>
+            <p>This PHONEBOOK has ${tamaño} people</p>
+            <p>${new Date().toDateString()} ${new Date().toTimeString()}</p>
+        </body>
+        </html>
+    `);
 });
 
 const PORT = 9000;
